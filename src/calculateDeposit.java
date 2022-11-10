15612ba9836da62177c92f8e57b17c1d3684f426
KirillGrunin <115705228+KirillGrunin@users.noreply.github.com>
2022-11-10 12:35:34 +0300

    Update calculateDeposit.java
    Изменил название класса

--- src/calculateDeposit.java
+++ src/calculateDeposit.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class calculateDeposit {
+public class DepositCalculator {
     private final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
